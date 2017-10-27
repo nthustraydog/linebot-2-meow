22dc62ec32e00d65bb0a0dc360f3facd2a1456b3
Jack <0805jauy9131253@gmail.com>
2017-10-28 03:01:12 +0800

    update

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ bot.on('message', function(event) {
   if (event.message.type == 'text') {
     let input = event.message.text.split(" ");
     if(input[0] === "@") {
-      let cmd = input[1].split(":");
+      let cmd = input[1].split("：");
 
       revise(cmd[0], cmd[1]);
     }
