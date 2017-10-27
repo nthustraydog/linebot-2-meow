4d988ede0eefdee8322824d08fe5c14fc0e7f7e5
Jack <0805jauy9131253@gmail.com>
2017-10-27 22:51:20 +0800

    test other reply

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ bot.on('message', function(event) {
       // error
       console.log('error');
     });
+
+    event.reply({
+      type: 'location',
+      title: 'my location',
+      address: '〒150-0002 東京都渋谷区渋谷２丁目２１−１',
+      latitude: 35.65910807942215,
+      longitude: 139.70372892916203
+  });
   }
 });
 
